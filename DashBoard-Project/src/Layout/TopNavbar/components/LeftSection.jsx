95c013fbfaa60d9f9b2ee09ad291057449ebb8de
Ahmed Hamood <55809640+HamoodRemx@users.noreply.github.com>
2021-12-24 02:44:15 +0400

    Delete DashBoard-Project directory

--- DashBoard-Project/src/Layout/TopNavbar/components/LeftSection.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function LeftSection() {
-  return (
-    <div className='topNavbar-Left-section'>
-      <h1 className='logo-title'>AdminSys</h1>
-    </div>
-  )
-}
--- DashBoard-Project/src/Pages/EditUser/components/EditTitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function EditTitle() {
-  return (
-    <div>
-      <h1 className='userEdit-main-title'> Edit User </h1>
-    </div>
-  )
-}
